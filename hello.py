39964e33c6531f27ba05c05a11581ac7128d86f7
bifinbabu <bifinbabu088@gmail.com>
2024-02-28 00:36:18 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+def add_three(input_var):
+    output_var = input_var + 3
+    return output_var
+
+
+# Run the function with 10 as input
+
+
+new_number = add_three(10)
+
+
+def test_function():
+    print("Hello")
+
+
+# Check that the value is 13, as expected
+print(new_number)
+test_function()
